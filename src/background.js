53192531315c146065540a152fc312dd759854fb
John Bernal <johnbernalfsd@gmail.com>
2025-04-22 14:22:09 -0600

    Adjustments

--- src/background.js
+++ src/background.js
@@ -1,10 +1,21 @@
-browser.pageAction.onClicked.addListener(async ({ id }) => {
-  const { result } = await browser.scripting.executeScript({
-    target: { tabId: id },
-    files: ["content.js"],
-  });
+/**
+ * Register the page action.
+ * Execute the content script when the page action is clicked.
+ */
+function registerPageAction() {
+  browser.pageAction.onClicked.addListener(async ({ id }) => {
+    const { result } = await browser.scripting.executeScript({
+      target: { tabId: id },
+      files: ["content.js"],
+    });
 
-  if (result) {
-    console.log("[PageAction/background/onClicked] Unexpected result:", result);
-  }
-});
+    if (result) {
+      console.log(
+        "[PageAction/background/onClicked] Unexpected result:",
+        result
+      );
+    }
+  });
+}
+
+registerPageAction();
